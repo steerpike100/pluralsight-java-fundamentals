c8f14f8059c1e7fd2cd39e883829a7da2a3a2211
stephenb <stephen.bartholomew@paxport.net>
2018-10-04 19:40:18 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = new MathEquation('d', 100d, 50.d);
+        equations[1] = new MathEquation('a', 25.0d, 92.0d);
+        equations[2] = new MathEquation('s', 225.0d, 17.0d);
+        equations[3] = new MathEquation('m', 11.0d, 3.0d);
+
+
+        for (MathEquation equation : equations) {
+            equation.execute();
+            System.out.println("The result = ");
+            System.out.println(equation.getResult());
+        }
+    }
+}
+
+
+
+
+
